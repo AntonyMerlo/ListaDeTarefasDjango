7d570b9ef09060c6ab7437c14dffbbaa732eaf97
AntonyMerlo <guilherme_antony.dutra@hotmail.com>
2024-11-07 15:28:33 -0300

    Validacoes de data e valor

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
+from django.utils.timezone import now
+def valideData(value):
+    if value < now().date():
+        raise ValidationError("A data limite não pode ser anterior ao dia de hoje.")
 
 class Tarefas(models.Model):
     idTarefas = models.IntegerField(primary_key=True)
     nomeTarefa = models.CharField(verbose_name="Nome da tarefa", max_length=40, unique=True, null=False, blank=False)
-    custo = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=False)
-    dataLimite = models.DateField(verbose_name="Data de Limite", null=False, blank=False)
+    custo = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=False, validators=[MinValueValidator(0)], default=0)
+    dataLimite = models.DateField(verbose_name="Data de Limite", null=False, blank=False, validators=[valideData])
     ordemDeApresentacao = models.IntegerField(null=True, blank=True)
 
     def save(self, *args, **kwargs):
